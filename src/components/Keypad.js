ac8bcaf4c8e5ce9580181f475432247c089b182c
Isaac <bennisaac24@gmail.com>
2025-01-21 12:41:38 +0300

    completed

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,14 @@
 // Code Keypad Component Here
 
 function Keypad (){
+    function handleInput (){
+        console.log("Entering password...")
+    }
+
     return (
-        <div></div>
+        <div>
+            <input type="password" placeholder="Enter Your Password!" onChange={handleInput} />
+        </div>
     )
 }
 
